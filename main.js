e1f11d3bc112406f9ba69339df2b32956b13cab0
SanjeevNarasimhan <69238368+SanjeevNarasimhan@users.noreply.github.com>
2020-09-07 19:51:42 +0530

    Added all the files required for the website

---
+++ main.js
@@ -0,0 +1,48 @@
+var about=document.getElementById("aboutus");
+var event=document.getElementById("events");
+var sync=document.getElementById("sync");
+var contacts=document.getElementById("contacts");
+about.onclick=function()
+{
+var image = document.getElementById('pic');
+image.src="download.jpg";
+let h1=document.getElementById("h1");
+h1.innerHTML="About ProfNitt";
+let p=document.getElementById("p");
+p.innerHTML="ProfNITT focuses on empowering students in the field of Finance and Investment by providing them knowledge, resources, and practical experience through collaborations and mentorship of an esteemed industry network sharing the same enthusiasm. We work in various domains of finance like Investment Banking, Private Equity and Venture Capital, Hedge Funds and Business Analytics."+"<br>"+"<br>"+"The belief that every student should learn to manage financial resources (including both personal and business-related) and plan them accordingly for the future, is the foundation of ProfNITT and we constantly strive towards it. We Started with publishing Content related to Finance and have grown into an active organization conducting various activities for the development of the Members and the Community."+"<br>"+"<br>"+"The industrial network is one of the major support systems of ProfNITT. Their constant brace has given us an environment of learning as well as practical experience. Helping us constantly with their video sessions and enlightening guest lectures, they have been our backbone in the journey of ProfNITT. The Alumni interactions that are conducted 2 per month for the team members focusses on the discussions of various domains of Finance and one-to-one interaction over video conferencing. We have brought in an investment of thousands in teams and provided mentorship to them through the Alumni Team. We are also involved in selecting the potential teams after due diligence to provide Seed-Funding to them via Alumni. We are conducting Knowledge Sharing Sessions for the team members and even for finance enthusiasts in NITT. We believe that content is one of the best ways to create financial literacy. We encourage students to learn a complicated topic in simple language on our Medium blog 'ProfNITT'. We have also taken initiatives of collaborating projects and case studies with firms and an Industrial Bootcamp at Chennai/Banglore which would help in getting experience and industrial exposure through multiple sessions, workshops, etc.";
+var sync=document.getElementById('sync');
+sync.innerHTML="To Know more about us visit our social media pages!.......";
+}
+events.onclick=function()
+{
+var image= document.getElementById('pic');
+image.src="talk.jpeg";
+let h1=document.getElementById("h1");
+h1.innerHTML="The Talk";
+let p =document.getElementById("p");
+p.innerHTML="Witness the stalwarts of the Finance Industry sharing their experience on how to climb the corporate ladder and changing the Dynamics of Finance."+"<br>"+"<br>"+"Every year we invite a Finance Leader to Campus to share their journey to the students and give them exposure from an Alumni who trod a different path and become the Stars of the Industry , moving and shaking up the regime."+"<br>"+"<br>"+"The first edition of the 'The Talk' saw ProfNITT's Industry Mentor, Mr. Rama Variankaval, Managing Director , JP Morgan , New York a prominent alumni of NITT and a very habile mentor to discuss his experiences and journey in the corporate world.";
+var sync=document.getElementById('sync');
+sync.innerHTML="More Events Coming Soon.......!";
+}
+sync.onclick=function()
+{
+var image = document.getElementById('pic');
+image.src="download.jpg";
+let h1=document.getElementById("h1");
+h1.innerHTML="About ProfNitt";
+let p=document.getElementById("p");
+p.innerHTML="ProfNITT focuses on empowering students in the field of Finance and Investment by providing them knowledge, resources, and practical experience through collaborations and mentorship of an esteemed industry network sharing the same enthusiasm. We work in various domains of finance like Investment Banking, Private Equity and Venture Capital, Hedge Funds and Business Analytics."+"<br>"+"<br>"+"The belief that every student should learn to manage financial resources (including both personal and business-related) and plan them accordingly for the future, is the foundation of ProfNITT and we constantly strive towards it. We Started with publishing Content related to Finance and have grown into an active organization conducting various activities for the development of the Members and the Community."+"<br>"+"<br>"+"The industrial network is one of the major support systems of ProfNITT. Their constant brace has given us an environment of learning as well as practical experience. Helping us constantly with their video sessions and enlightening guest lectures, they have been our backbone in the journey of ProfNITT. The Alumni interactions that are conducted 2 per month for the team members focusses on the discussions of various domains of Finance and one-to-one interaction over video conferencing. We have brought in an investment of thousands in teams and provided mentorship to them through the Alumni Team. We are also involved in selecting the potential teams after due diligence to provide Seed-Funding to them via Alumni. We are conducting Knowledge Sharing Sessions for the team members and even for finance enthusiasts in NITT. We believe that content is one of the best ways to create financial literacy. We encourage students to learn a complicated topic in simple language on our Medium blog 'ProfNITT'. We have also taken initiatives of collaborating projects and case studies with firms and an Industrial Bootcamp at Chennai/Banglore which would help in getting experience and industrial exposure through multiple sessions, workshops, etc.";
+var sync=document.getElementById('sync');
+sync.innerHTML="To Know more about us visit our social media pages!.......";
+}
+contacts.onclick=function()
+{
+    var image = document.getElementById('pic');
+image.src="call.jpg";
+let h1=document.getElementById("h1");
+h1.innerHTML="Contact us";
+let p=document.getElementById("p");
+p.innerHTML="<br>"+"Shagun- +91 97789 60737"+"<br>"+"Satyam- +91 97418 87412"+"<br>"+"Kumaresh- +91 73737 66520";
+var sync=document.getElementById('sync');
+sync.innerHTML="Thank You!.......";
+}
